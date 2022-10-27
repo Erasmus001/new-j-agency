2808130c3b2c6c46300112e6d8e8b4ac3c893fe4
Erasmus <erasmusmensah00@gmail.com>
2022-10-27 16:51:18 +0000

    update: added the footer and get-started page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,25 @@
+const backedVideo = document.getElementById("backedVideo");
+const playVidBtn = document.getElementById("playVidBtn");
+
 const getTestimonials = async () => {
-  const data = await fetch('/src/scripts/data.json');
+  const data = await fetch("/src/scripts/data.json");
   const res = await data.json();
 
-  console.log('====================================');
+  console.log("====================================");
   console.log(res[0].Testimonials);
-  console.log('====================================');
-}
+  console.log("====================================");
+};
 
-getTestimonials()
+// getTestimonials();
+let isPlaying = false;
+
+playVidBtn.addEventListener("click", () => {
+  if(!isPlaying) {
+    backedVideo.play();
+    // isPlaying = true;
+    playVidBtn.classList.add('hidden')
+  } else {
+    backedVideo.pause()
+    // isPlaying = false;
+  }
+});
