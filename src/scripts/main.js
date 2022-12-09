2ad9b2f6e54886872ff9d7681a224519757c1a1c
Erasmus <erasmusmensah00@gmail.com>
2022-12-09 00:21:23 +0000

    commented all codes in js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,25 @@
-const backedVideo = document.getElementById("backedVideo");
-const playVidBtn = document.getElementById("playVidBtn");
+// const backedVideo = document.getElementById("backedVideo");
+// const playVidBtn = document.getElementById("playVidBtn");
 
-const getTestimonials = async () => {
-  const data = await fetch("/src/scripts/data.json");
-  const res = await data.json();
+// const getTestimonials = async () => {
+//   const data = await fetch("/src/scripts/data.json");
+//   const res = await data.json();
 
-  console.log("====================================");
-  console.log(res[0].Testimonials);
-  console.log("====================================");
-};
+//   console.log("====================================");
+//   console.log(res[0].Testimonials);
+//   console.log("====================================");
+// };
 
-// getTestimonials();
-let isPlaying = false;
+// // getTestimonials();
+// let isPlaying = false;
 
-playVidBtn.addEventListener("click", () => {
-  if(!isPlaying) {
-    backedVideo.play();
-    // isPlaying = true;
-    playVidBtn.classList.add('hidden')
-  } else {
-    backedVideo.pause()
-    // isPlaying = false;
-  }
-});
+// playVidBtn.addEventListener("click", () => {
+//   if(!isPlaying) {
+//     backedVideo.play();
+//     // isPlaying = true;
+//     playVidBtn.classList.add('hidden')
+//   } else {
+//     backedVideo.pause()
+//     // isPlaying = false;
+//   }
+// });
